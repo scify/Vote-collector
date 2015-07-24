fbd9e837ec9dedbb9df4162661b6373a48a685f0
leots <leotsekouras@gmail.com>
2015-07-24 12:15:05 +0300

    Group info page now shows how many members there are in the group

--- resources/views/groups/show.blade.php
+++ resources/views/groups/show.blade.php
@@ -4,30 +4,35 @@
     <h1>{{$group->name}}</h1>
 
     @if (count($members) > 0)
-        <h2>Μέλη:</h2>
+        <h2>Μέλη: {{ count($members) }}</h2>
 
-        <div class="table-responsive">
-            <table class="table table-condensed table-striped">
-                <thead>
-                    <tr>
-                        <th>Όνομα</th>
-                        <th>Επώνυμο</th>
-                        <th>Ενέργειες</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($members as $member)
+        <div class="panel panel-default">
+            <div class="panel-heading">Ονόματα μελών</div>
+
+            <div class="table-responsive">
+                <table class="table table-condensed table-striped">
+                    <thead>
                         <tr>
-                            <td>{{ $member->first_name }}</td>
-                            <td>{{ $member->last_name }}</td>
-                            <td> {{-- Info button --}}
-                                @include('partials.infoBtn', ['url' => '/members/' . $member->id])
-                            </td>
+                            <th>Όνομα</th>
+                            <th>Επώνυμο</th>
+                            <th>Ενέργειες</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach($members as $member)
+                            <tr>
+                                <td>{{ $member->first_name }}</td>
+                                <td>{{ $member->last_name }}</td>
+                                <td> {{-- Info button --}}
+                                    @include('partials.infoBtn', ['url' => '/members/' . $member->id])
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
+
     @else
         <p>
             Αυτή η κοινοβουλευτική ομάδα δεν έχει κανένα μέλος!
