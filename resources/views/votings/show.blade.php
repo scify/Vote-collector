4daa9094fa67b49cbfea7e8fe4268c10dc81d211
leots <leotsekouras@gmail.com>
2015-07-24 18:05:06 +0300

    Add chart to voting results (wip)

--- resources/views/votings/show.blade.php
+++ resources/views/votings/show.blade.php
@@ -1,5 +1,63 @@
 @extends('app')
 
+@section('head')
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+
+    <script type="text/javascript">
+        google.load("visualization", "1", {packages:["corechart"]});
+        google.setOnLoadCallback(drawChart);
+            function drawChart() {
+
+            // Gather data from table
+            var answerTds = [];
+            $('.table').find('tr').each(function(index, tr) {
+                answerTds.push($(tr).children('td')[1]);
+            });
+
+            //todo: write comments
+            var answerLabels = [];
+            var answerCounts = [];
+            $(answerTds).each(function(index, td) {
+                var answerText = $(td).text().replace(/ /g,'');
+
+                if (answerText.length > 0) {
+                    var exists = false;
+                    $(answerLabels).each(function(index, label) {
+                        if (label == answerText) {
+                            exists = true;
+                        }
+                    });
+
+                    if (exists) {
+                        var index = answerLabels.indexOf(answerText);
+                        answerCounts[index] += 1;
+                    } else {
+                        answerLabels.push(answerText);
+                        answerCounts.push(1);
+                    }
+                }
+            });
+
+            var mydata = [];
+            mydata.push(['Απάντηση', 'Ψήφοι']);
+            $(answerLabels).each(function(index, label) {
+                mydata.push([label, answerCounts[index]]);
+            });
+
+            var data = google.visualization.arrayToDataTable(mydata);
+
+            var options = {
+                title: 'Ψήφοι'
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+            chart.draw(data, options);
+        }
+    </script>
+
+@stop
+
 @section('content')
     <h1>Προβολή ψηφοφορίας</h1>
 
@@ -19,6 +77,8 @@
         @endif
     </div>
 
+    <!-- google chart test -->
+    <div id="piechart" style="width: 900px; height: 500px;"></div>
 
     {{-- Check if the voting has any votes, and show them --}}
     @if(count($memberVotes) > 0)
@@ -45,6 +105,6 @@
                     </tbody>
                 </table>
             </div>
-</div>
+        </div>
     @endif
 @stop
