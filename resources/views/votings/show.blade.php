b883220fa6a9df0400dbfae6687a6080d62ea0bb
leots <leotsekouras@gmail.com>
2015-07-27 10:53:10 +0300

    Moved Google Charts script into its own file and added some comments to it

--- resources/views/votings/show.blade.php
+++ resources/views/votings/show.blade.php
@@ -2,60 +2,7 @@
 
 @section('head')
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-
-    <script type="text/javascript">
-        google.load("visualization", "1", {packages:["corechart"]});
-        google.setOnLoadCallback(drawChart);
-            function drawChart() {
-
-            // Gather data from table
-            var answerTds = [];
-            $('.table').find('tr').each(function(index, tr) {
-                answerTds.push($(tr).children('td')[1]);
-            });
-
-            //todo: write comments
-            var answerLabels = [];
-            var answerCounts = [];
-            $(answerTds).each(function(index, td) {
-                var answerText = $(td).text().replace(/ /g,'');
-
-                if (answerText.length > 0) {
-                    var exists = false;
-                    $(answerLabels).each(function(index, label) {
-                        if (label == answerText) {
-                            exists = true;
-                        }
-                    });
-
-                    if (exists) {
-                        var index = answerLabels.indexOf(answerText);
-                        answerCounts[index] += 1;
-                    } else {
-                        answerLabels.push(answerText);
-                        answerCounts.push(1);
-                    }
-                }
-            });
-
-            var mydata = [];
-            mydata.push(['Απάντηση', 'Ψήφοι']);
-            $(answerLabels).each(function(index, label) {
-                mydata.push([label, answerCounts[index]]);
-            });
-
-            var data = google.visualization.arrayToDataTable(mydata);
-
-            var options = {
-                title: 'Ψήφοι'
-            };
-
-            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-            chart.draw(data, options);
-        }
-    </script>
-
+    <script src="{{ URL::asset('js/votingChart.js') }}"></script>
 @stop
 
 @section('content')
@@ -77,8 +24,8 @@
         @endif
     </div>
 
-    <!-- google chart test -->
-    <div id="piechart" style="width: 900px; height: 500px;"></div>
+    {{-- Google Chart --}}
+    <div id="piechart" style="width: 900px; height: 500px; display: block; margin: 0 auto;"></div>
 
     {{-- Check if the voting has any votes, and show them --}}
     @if(count($memberVotes) > 0)
