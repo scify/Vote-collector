26629b59d11c8d109bb7d6eeed823a0148eeec4e
leots <leotsekouras@gmail.com>
2015-08-05 18:02:06 +0300

    Removed some old commented code

--- resources/views/votings/partials/answersform.blade.php
+++ resources/views/votings/partials/answersform.blade.php
@@ -6,6 +6,7 @@
     {!! Form::hidden('votingItems[]', $vItem->id) !!}
 @endforeach
 
+{{-- Table with default answer select inputs --}}
 <div class="table-responsive">
     <table class="table table-striped">
         <thead>
@@ -31,9 +32,5 @@
     </table>
 </div>
 
-{{--<div class="form-group">--}}
-    {{--{!! Form::label('answer_' . $group->id, $group->name) !!}
-    {!! Form::select('answer_' . $group->id, $voting->type->answers->lists('answer', 'id'), isset($edit)?$group->defaultAnswer($voting->id):null, ['class' => 'form-control selectpicker']) !!}--}}
-{{--</div>--}}
 {!! Form::submit('Αποθήκευση', ['class' => 'btn btn-primary']) !!}
 <a href="{{ url('votings') }}" class="btn btn-default">Άκυρο</a>
