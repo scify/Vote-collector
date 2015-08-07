aa45a11be73a8f3c4c3bb527ab46f7e4e5ce8944
leots <leotsekouras@gmail.com>
2015-08-07 12:08:49 +0300

    Readings page does not use laravel illuminate form since it's not needed

--- resources/views/votings/reading.blade.php
+++ resources/views/votings/reading.blade.php
@@ -7,50 +7,49 @@
 @section('content')
     <h1 id="title" class="page-header">Πρώτη ανάγνωση <small>{{ $votingTitle }}</small></h1>
 
-    {!! Form::open(['action' => 'VotingsController@saveAnswers', 'class' => 'form-horizontal', 'id' => 'votesform', 'data-votingid' => $votingid]) !!}
-        <div class="table-responsive">
-            <table class="table">
-                <thead>
-                    <th>Βουλευτής</th>
-                    @foreach($myVotingItems as $vi)
-                        <th>{{ $vi['title'] }}</th>
-                    @endforeach
-                    <th>Ενέργειες</th>
-                </thead>
-                <tbody>
-                    @foreach($myMembers as $member)
-                        <tr class="member" data-saved="{{ $member['isSaved'] }}" data-id="{{ $member['id'] }}" data-wassavedasabsent="{{ $member['isAbsent'] }}">
-                            <td class="memberName col-sm-3">
-                                {{ $member['full_name'] }}
-                            </td>
-                            @foreach($myVotingItems as $vi)
-                                <td class="votingItem" data-id="{{ $vi['id'] }}">
-                                    <div class="radios hidden">
-                                        @foreach($vi['answers'] as $answerId => $answer)
-                                            {!! Form::radio('answer_' . $member['id'] . $vi['id'], $answerId, $member['answerIds'][$vi['id']] == $answerId, ['id' => 'rd' . $member['id'] . $vi['id'] . $answerId]) !!}
-                                            {!! Form::label('rd' . $member['id'] . $vi['id'] . $answerId, $answer, ['class' => 'control-label']) !!}
-                                            <br>
-                                        @endforeach
-                                    </div>
+    {{-- votesform is now votesDiv --}}
+    <div id="votesDiv" class="table-responsive" data-votingid="{{ $votingid }}">
+        <table class="table">
+            <thead>
+                <th>Βουλευτής</th>
+                @foreach($myVotingItems as $vi)
+                    <th>{{ $vi['title'] }}</th>
+                @endforeach
+                <th>Ενέργειες</th>
+            </thead>
+            <tbody>
+                @foreach($myMembers as $member)
+                    <tr class="member" data-saved="{{ $member['isSaved'] }}" data-id="{{ $member['id'] }}" data-wassavedasabsent="{{ $member['isAbsent'] }}">
+                        <td class="memberName col-sm-3">
+                            {{ $member['full_name'] }}
+                        </td>
+                        @foreach($myVotingItems as $vi)
+                            <td class="votingItem" data-id="{{ $vi['id'] }}">
+                                <div class="radios "> {{--hidden--}}
+                                    @foreach($vi['answers'] as $answerId => $answer)
+                                        <input type="radio" value="{{ $answerId }}" id="rd{{ $member['id'] . $vi['id'] . $answerId }}" name="answer_{{ $member['id'] . $vi['id'] }}" {{ ($member['answerIds'][$vi['id']] == $answerId) ? 'checked=checked' : '' }}>
+                                        <label for="rd{{ $member['id'] . $vi['id'] . $answerId }}" class="control-label">{{ $answer }}</label>
+                                        <br>
+                                    @endforeach
+                                </div>
 
-                                    {{-- Label that will display the selected answer --}}
-                                    <span id="selAnswerLabel{{ $member['id'] . $vi['id'] }}" class="label label-primary pull-left">{{ $member['labels'][$vi['id']] }}</span>
-                                </td>
-                            @endforeach
-                            <td class="btnCell">
-                                {{-- buttons will be added here when needed by javascript --}}
+                                {{-- Label that will display the selected answer --}}
+                                <span id="selAnswerLabel{{ $member['id'] . $vi['id'] }}" class="label label-primary pull-left">{{ $member['labels'][$vi['id']] }}</span>
                             </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                        @endforeach
+                        <td class="btnCell">
+                            {{-- buttons will be added here when needed by javascript --}}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
-        <a href="{{ url('votings') }}" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left"></span> Πίσω</a>
-        <div id="readingsButtonGroup" class="btn-group">
-            <a href="#" id="nextPhaseBtn" class="btn btn-primary"><span class="glyphicon glyphicon-forward"></span> Δεύτερη ανάγνωση</a>
-        </div>
-    {!! Form::close() !!}
+    <a href="{{ url('votings') }}" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left"></span> Πίσω</a>
+    <div id="readingsButtonGroup" class="btn-group">
+        <a href="#" id="nextPhaseBtn" class="btn btn-primary"><span class="glyphicon glyphicon-forward"></span> Δεύτερη ανάγνωση</a>
+    </div>
 @stop
 
 @section('footer')
